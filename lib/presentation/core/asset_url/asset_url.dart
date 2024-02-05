fa8744aad959baffb7c7c90c5470646b2342446e
Akshay <akshayashok38@gmail.com>
2024-02-05 16:03:12 +0530

    Implement demo profile page

--- lib/presentation/core/asset_url/asset_url.dart
+++ lib/presentation/core/asset_url/asset_url.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: constant_identifier_names
 
 class AssetUrls {
-  static const String LOGO = 'assets/logo_pic.png';
+  static const String LOGO = 'assets/kel_logo.png';
   static const String KSHB_LOGO = 'assets/kshb_logo.png';
+  static const String BUILDING_LOGO = 'assets/building6.png';
 }
